e7cb51d51ae7533f59644f4e3b5d19386c279f9a
tramntm <49011241+tramntm@users.noreply.github.com>
2020-04-23 11:40:38 +0700

    Update api.php

--- api.php
+++ api.php
@@ -3,88 +3,69 @@
 
 require 'restful_api.php';
 
-class api extends restful_api {
+class api extends restful_api 
+{
 
-    function __construct(){
+    function __construct()
+    {
         parent::__construct();
     }
 
-    function checkyear(){
-        if ($this->method == 'GET'){
-            $this->response(200, $this->getyear($this->params));
-        }
-    }
-
-    function checkptbac2()
+    function checkngayketiep()
     {
-        if($this->method == 'GET')
+        if ($this->method == 'GET')
         {
-            $this->response(200,$this->getptbac2($this->params));
+            $this->response(200, $this->getngayketiep($this->params));
         }
     }
 
-    function getptbac2($params)
+    function getngayketiep($params)
     {
         if(empty($params[0])||empty($params[1])||empty($params[2])||!empty($params[3]))
         {
-            return array("status" => false, "data" => array());
-        }
-        else{
-            $a=(double)$params[0];
-            $b=(double)$params[1];
-            $c=(double)$params[2];
-            $denta = $b*$b - 4*$a*$c;
-            if($a==0)
-            {
-                return array("status" => false, "data" => array());
-            }
-            elseif($denta<0)
-            {
-                $x1 = null;
-                $x2 = null;
-                $data = "Phuong trinh vo nghiem";
-                return array("status" => true,"data" => array("x1"=>$x1,"x2"=>$x2,"result"=>$data));
-            }
-            elseif($denta==0)
-            {
-                $x1 = (double) round(-$b/(2*$a),2);
-                $x2 = (double) round(-$b/(2*$a),2);
-                $data = "Phuong trinh co nghiem kep";
-                return array("status" => true,"data" => array("x1"=>$x1,"x2"=>$x2,"result"=>$data));
-            }
-            elseif ($denta>0)
-            {
-                $x1 = (double) round((-$b-sqrt($denta))/(2*$a),2);
-                $x2 = (double) round((-$b+sqrt($denta))/(2*$a),2);
-                $data = "Phuong trinh vo nghiem";
-                return array("status" => true,"data" => array("x1"=>$x1,"x2"=>$x2,"result"=>$data));
-            }
-        }
-    }
-
-    function getyear($params)
-    {
-        if(empty($params[0])||!empty($params[1]))
-        {
-            $data = array("status" => false, "data" => array());
-            return $data;
-        }elseif((double)$params[0]-(int)$params[0]!=0||!(int)$params[0]>0)
-        {
-            $data = array("status" => false, "data" => array());
-            return $data;
+           return array("status" => false, "data" => array());
         }
         else
         {
-            $x = (int)$params[0];
-            if ($x % 400 == 0 || $x % 4 == 0 && $x % 100 != 0) {
-                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " la nam nhuan"));
+            $d=(double)$params[0];
+            $m=(double)$params[1];
+            $y=(double)$params[2];
+            if ($d > 31 || $m >12) {
+            return $data = "Nhap ngay/thang khong dung";
 
-            } else {
-                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " khong phai la nam nhuan"));
+            } 
+            elseif (($d == 31 && $m == 2)||($d == 30 && $m == 2)||($d == 31 && $m == 4)||($d == 31 && $m == 6)||($d == 31 && $m == 9)||($d == 31 && $m == 11)) 
+            {
+               return $data = "Nhap ngay khong dung voi thang";
             }
-            return $data;
+            elseif (($d == 31 && $m == 1)||($d == 31 && $m == 3)||($d == 31 && $m == 5)||($d == 31 && $m == 7)|| ($d == 31 && $m == 8)||($d == 31 && $m == 10)||($d == 31 && $m == 12)||($d == 28 && $m == 2)||($d == 30 && $m == 4)||($d == 30 && $m == 6)||($d == 30 && $m == 9)||($d == 30 && $m == 11)) 
+            {
+               if($m ==12)
+               {
+                   $ngay = 1;
+                   $thang =1;
+                   $nam = $y +1;
+                   return $data= "Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
+               }
+                else 
+                {
+                    $ngay = 1;
+                    $thang =$m +1;
+                    $nam = $y;
+                   return $data= "Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
+                }                
+            }
+            else
+            {
+                    $ngay = $d+1;
+                    $thang = $m;
+                    $nam = $y;
+                    return $data= "Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
+            }
         }
     }
+
+   
 }
 
 $user_api = new api();
